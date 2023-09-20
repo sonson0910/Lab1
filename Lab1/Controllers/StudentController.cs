4d0e7b77d68e1b5225ceee3656871638bf33256a
Son <sonlearn155@gmail.com>
2023-09-20 10:02:14 +0700

    Lab2

---
+++ Lab1/Controllers/StudentController.cs
@@ -0,0 +1,58 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+namespace Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        [Route("Admin/Student/List")]
+        public IActionResult Index() { return View(listStudents); }
+
+
+        private List<Student> listStudents = new List<Student>()
+        {
+            new Student() {Id = 101, Name = "Son", Branch = Branch.IT, Gender = Gender.Female, IsRegular = true, Address = "433", Email = "Sonlearn155@gmail.com"},
+            new Student() { Id = 101, Name = "Hải Nam", Branch = Branch.IT,
+            Gender = Gender.Male, IsRegular=true,
+            Address = "A1-2018", Email = "nam@g.com" },
+
+            new Student() { Id = 102, Name = "Minh Tú", Branch = Branch.BE,
+            Gender = Gender.Female, IsRegular=true,
+            Address = "A1-2019", Email = "tu@g.com" },
+
+            new Student() { Id = 103, Name = "Hoàng Phong", Branch = Branch.CE,
+            Gender = Gender.Male, IsRegular=false,
+            Address = "A1-2020", Email = "phong@g.com" },
+
+            new Student() { Id = 104, Name = "Xuân Mai", Branch = Branch.EE,
+            Gender = Gender.Female, IsRegular = false,
+            Address = "A1-2021", Email = "mai@g.com" }
+        };
+
+
+        [HttpGet("Admin/Student/List")]
+        public IActionResult Create()
+        {
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem {Text = "IT", Value = "1"},
+                new SelectListItem {Text = "BE", Value="2"},
+                new SelectListItem{Text = "CE", Value = "3"},
+                new SelectListItem{Text = "EE", Value = "4"}
+            };
+            return View();
+        }
+
+        [HttpPost("Admin/Student/List")]
+        public IActionResult Create(Student s)
+        {
+            s.Id = listStudents.Last<Student>().Id + 1;
+            listStudents.Add(s);
+            return View("Index", listStudents);
+        }
+
+    }
+}
