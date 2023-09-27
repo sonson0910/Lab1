f4e9af85f7bb3f6dcd196a6ee3a892c4b9adaf1a
Son <sonlearn155@gmail.com>
2023-09-27 17:06:39 +0700

    version 3.0

--- Lab1/Controllers/StudentController.cs
+++ Lab1/Controllers/StudentController.cs
@@ -7,30 +7,31 @@ namespace Lab1.Controllers
 {
     public class StudentController : Controller
     {
-        [Route("Admin/Student/List")]
-        public IActionResult Index() { return View(listStudents); }
+        [Route("Admin/Student/Index")]
+        public IActionResult Index() { return View(ListStudents); }
 
 
         private List<Student> listStudents = new List<Student>()
         {
-            new Student() {Id = 101, Name = "Son", Branch = Branch.IT, Gender = Gender.Female, IsRegular = true, Address = "433", Email = "Sonlearn155@gmail.com"},
+            new Student() {Id = 101, Name = "Son", Branch = Branch.IT, Gender = Gender.Female, IsRegular = true, Address = "433", Email = "Sonlearn155@gmail.com", Diem = 9.4},
             new Student() { Id = 101, Name = "Hải Nam", Branch = Branch.IT,
             Gender = Gender.Male, IsRegular=true,
-            Address = "A1-2018", Email = "nam@g.com" },
+            Address = "A1-2018", Email = "nam@g.com", Diem = 8.2},
 
             new Student() { Id = 102, Name = "Minh Tú", Branch = Branch.BE,
             Gender = Gender.Female, IsRegular=true,
-            Address = "A1-2019", Email = "tu@g.com" },
+            Address = "A1-2019", Email = "tu@g.com", Diem = 7.6 },
 
             new Student() { Id = 103, Name = "Hoàng Phong", Branch = Branch.CE,
             Gender = Gender.Male, IsRegular=false,
-            Address = "A1-2020", Email = "phong@g.com" },
+            Address = "A1-2020", Email = "phong@g.com" , Diem = 8.0},
 
             new Student() { Id = 104, Name = "Xuân Mai", Branch = Branch.EE,
             Gender = Gender.Female, IsRegular = false,
-            Address = "A1-2021", Email = "mai@g.com" }
+            Address = "A1-2021", Email = "mai@g.com", Diem = 3.6 }
         };
 
+        public List<Student> ListStudents { get => listStudents; set => listStudents = value; }
 
         [HttpGet("Admin/Student/List")]
         public IActionResult Create()
@@ -45,14 +46,15 @@ namespace Lab1.Controllers
             };
             return View();
         }
+        
         [HttpPost("Admin/Student/List")]
         public IActionResult Create(Student s)
         {
             if (ModelState.IsValid)
             {
-                s.Id = listStudents.Last<Student>().Id + 1;
-                listStudents.Add(s);
-                return View("Index", listStudents);
+                s.Id = ListStudents.Last().Id + 1;
+                ListStudents.Add(s);
+                return View("Index", ListStudents);
             }
             ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
             ViewBag.AllBranches = new List<SelectListItem>()
@@ -64,5 +66,7 @@ namespace Lab1.Controllers
             };
             return View();
         }
+
+        
     }
 }
--- Lab1/Models/Student.cs
+++ Lab1/Models/Student.cs
@@ -6,42 +6,44 @@ namespace Lab1.Models
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Bạn phải nhập tên")]
-        [StringLength(100, MinimumLength = 4, ErrorMessage = "Bạn phải nhập tên có độ dài từ 4-100 kí tự")]
+        [Required(ErrorMessage = "Bạn cần nhập tên")]
+        [StringLength(100, MinimumLength = 4, 
+            ErrorMessage = "Bạn cần nhập tên có độ dài từ 4-100 kí tự")]
         public string? Name { get; set; }
 
         [Required(ErrorMessage = "Bạn phải nhập email")]
         [RegularExpression(@"^[\w-]+(\.[\w-]+)*@gmail\.com$",
-        ErrorMessage = "Bạn phải nhập email có đuôi là gmail.com")]
+            ErrorMessage = "Bạn cần nhập email có đuôi là @gmail.com")]
         public string? Email {  get; set; }
 
         [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$",
-        ErrorMessage = "Mật khẩu phải từ 8 ký tự trở lên, có ít nhất một chữ cái viết hoa, một chữ cái viết thường, một chữ số và một ký tự đặc biệt.")]
+            ErrorMessage = "Mật khẩu của bạn phải từ 8 ký tự trở lên, có ít nhất một chữ cái viết hoa, một chữ cái viết thường, một chữ số và một ký tự đặc biệt.")]
         [Required(ErrorMessage = "Bạn phải nhập password")]
         public string? Password {  get; set; }
 
 
-        [Required(ErrorMessage = "Bạn phải nhập Branch")]
+        [Required(ErrorMessage = "Bạn cần nhập Branch")]
         public Branch? Branch { get; set; }
 
-        [Required(ErrorMessage = "Bạn phải nhập giới tính")]
+        [Required(ErrorMessage = "Bạn chọn nhập giới tính")]
         public Gender? Gender { get; set; }
-
 
         public bool IsRegular {  get; set; }
 
         [DataType(DataType.MultilineText)]
-        [Required(ErrorMessage = "Bạn phải nhập Bạn phải nhập địa chỉ")]
+        [Required(ErrorMessage = "Bạn cần nhập địa chỉ")]
         public string? Address {  get; set; }
 
-        [Range(typeof(DateTime), "1/1/1963", "12/31/2023", ErrorMessage = "Ngày sinh phải nằm trong khoảng từ 01/01/1963 đến 31/12/2023")]
+        [Range(typeof(DateTime), "1/1/1920", "12/31/2023", 
+            ErrorMessage = "Ngày sinh cần phải nằm trong khoảng thời gian từ 01/01/1920 đến 31/12/2023")]
         [DataType(DataType.Date)]
         [Required(ErrorMessage = "Bạn phải nhập ngày sinh")]
         public DateTime DateOfBorth { get; set; }
 
-        [Range(typeof(double), "0.0", "10.0", ErrorMessage = "Bạn phải nhập điểm trong khoảng 0.0- 10.0")]
+        [Range(typeof(double), "0.0", "10.0", 
+            ErrorMessage = "Bạn cần nhập điểm trong khoảng 0 - 10 theo kiểu số thực, lấy một chữ số thập phân đằng sau")]
         // đùng data type
-        [Required(ErrorMessage = "Bạn phải nhập điểm")]
+        [Required(ErrorMessage = "Bạn cần nhập điểm")]
         public double? Diem { get; set; }
     }
 }
